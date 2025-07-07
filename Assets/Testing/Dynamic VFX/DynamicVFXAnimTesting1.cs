5eb5d8459e7c55a91972b29c54e2aa0277b3d42e
arghhhhh <gitlinjoss@tutanota.com>
2025-07-06 22:33:55 -0400

    dev log -7/6

---
+++ Assets/Testing/Dynamic VFX/DynamicVFXAnimTesting1.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DynamicVFXAnimTesting1 : MonoBehaviour
+{
+    public VisualEffect vfx;             // Assign in Inspector
+    public string spawnParameter = "SpawnOn";
+    public string spawnAnimStartTimeParameter = "SpawnAnimStartTime";
+    public KeyCode spawnToggleKey = KeyCode.S;
+
+    private bool spawnAnimDirection = false;
+
+    public string moveParameter = "MoveToEnd";
+    public string moveAnimStartTimeParameter = "MoveAnimStartTime";
+    public KeyCode moveToggleKey = KeyCode.M;
+
+    private bool moveAnimDirection = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(spawnToggleKey))
+        {
+            spawnAnimDirection = !spawnAnimDirection;
+            vfx.SetBool(spawnParameter, spawnAnimDirection);
+            vfx.SetFloat(spawnAnimStartTimeParameter, Time.time);
+            Debug.Log("SpawnOn set to: " + spawnAnimDirection);
+        }
+        if (Input.GetKeyDown(moveToggleKey))
+        {
+            moveAnimDirection = !moveAnimDirection;
+            vfx.SetBool(moveParameter, moveAnimDirection);
+            vfx.SetFloat(moveAnimStartTimeParameter, Time.time);
+            Debug.Log("MoveToEnd set to: " + moveAnimDirection);
+        }
+    }
+}
---
+++ Assets/Testing/Dynamic VFX/DynamicVFXAnimTesting2.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DynamicVFXAnimTesting2 : MonoBehaviour
+{
+    public VisualEffect vfx;             // Assign in Inspector
+    public string DirectionParameter = "Direction";
+    public string StartTimeParameter = "StartTime";
+    public string prevStartTimeParameter = "PrevStartTime";
+    public string prevValueParameter = "PrevValue";
+    public string currentValueParameter = "CurrentValue";
+    public KeyCode animToggleKey = KeyCode.S;
+
+    private bool animDirection = false;
+    private float prevStartTime = 0f;    // Track the previous start time
+    private Vector4 prevValue = Vector4.zero;        // Track the previous value
+
+    void Start()
+    {
+        prevStartTime = vfx.GetFloat(StartTimeParameter);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(animToggleKey))
+        {
+            // Store the current start time as the previous start time
+            prevStartTime = vfx.GetFloat(StartTimeParameter);
+            prevValue = vfx.GetVector4(currentValueParameter);
+            
+            animDirection = !animDirection;
+            vfx.SetBool(DirectionParameter, animDirection);
+            vfx.SetFloat(StartTimeParameter, Time.time);
+            vfx.SetVector4(currentValueParameter, new Vector4(1, 2, 3, 4));
+            vfx.SetFloat(prevStartTimeParameter, prevStartTime);
+            vfx.SetVector4(prevValueParameter, prevValue);
+            Debug.Log("AnimOn set to: " + animDirection);
+        }
+    }
+}
